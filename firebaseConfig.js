3d989378ee5dd135f96f90e02e20523873462412
Timi-Duban <ti.mi.34@hotmail.fr>
2024-01-23 16:33:13 +0200

    fix firestore emulator on android

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -16,6 +16,8 @@ const firebaseConfig = {
     measurementId: "G-BNK8S8HE79"
 };
 
+const baseEmulatorAddress = Platform.OS === 'android' ? '10.0.2.2' : '127.0.0.1';
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -37,18 +39,14 @@ initializeAuth(app, { persistence });
 const auth = getAuth(app);
 if (process.env.EXPO_PUBLIC_EMULATE_AUTH === 'emulate') {
     console.log('Emulating auth service')
-    if (Platform.OS === 'android') {
-        connectAuthEmulator(auth, "http://10.0.2.2:9099");
-    } else {
-        connectAuthEmulator(auth, "http://127.0.0.1:9099");
-    }
+    connectAuthEmulator(auth, 'http://' + baseEmulatorAddress + ':9099');
 }
 
 // Initialize Firestore
 const db = getFirestore(app);
 if (process.env.EXPO_PUBLIC_EMULATE_FIRESTORE === 'emulate') {
     console.log('Emulating firestore service')
-    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+    connectFirestoreEmulator(db, baseEmulatorAddress, 8080);
 }
 
 export { app, auth, db };
