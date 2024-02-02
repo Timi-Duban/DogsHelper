a1fb32c975f7c6883bab45013156fb76d7c2f346
Timi-Duban <ti.mi.34@hotmail.fr>
2024-02-02 10:46:57 +0200

    fix react-native-paper provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
     return (
         <SafeAreaView style={{flex: 1}}>
+            <PaperProvider>
             <Slot />
             <StatusBar style="auto" />
+            </PaperProvider>
         </SafeAreaView>
     );
 }
